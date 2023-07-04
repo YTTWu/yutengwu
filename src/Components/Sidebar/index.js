d31df9de84f824825e3d716f83cbd78c81697b84
Yuteng Terrence Wu <yutengwu@yahoo.com>
2023-07-03 17:57:11 -0700

    home page

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -3,7 +3,13 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+    faLinkedin,
+    faGithub,
+    faYoutube,
+    faSkype,
+  } from '@fortawesome/free-brands-svg-icons'
 
 
 
@@ -20,7 +26,31 @@ const Sidebar = () => (
             <NavLink exact="true" activeclassname="avtive" className="about-link" to="/about">
                 <FontAwesomeIcon icon={ faUser } color="#4d4d4e" />
             </NavLink>
+            <NavLink exact="true" activeclassname="avtive" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={ faEnvelope } color="#4d4d4e" />
+            </NavLink>
         </nav>
+        <ul className='nav-linkedin'>
+            <li>
+                <a  
+                    
+                    href="https://www.linkedin.com/in/yutengwu/"
+                    rel="linkedin"
+                    target='_blank'
+                >
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+            </li>
+            <li>
+                <a  
+                    href="https://github.com/YTTWu"
+                    rel="github"
+                    target='_blank'
+                >
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
