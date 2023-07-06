f9e30b8bd4488f91776d3fad41b39cff780aff3d
Yuteng Terrence Wu <yutengwu@yahoo.com>
2023-07-06 12:30:09 -0700

    responsive web design

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -42,7 +42,7 @@ const Home = () => {
 
                     />
                 </h1>
-                <h2> JavaScript / C++ / Python / HTML/CSS / React.js / Vue/js </h2>
+                <h2> JavaScript / C++ / Python / HTML/CSS / React.js / Node.js </h2>
                 <Link to="/contact" className="flat-button">Contact me</Link>
             </div>
             <Logo />
