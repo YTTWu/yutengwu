ea58390dc45895339f5edda4e572e9d93670492b
Yuteng Terrence Wu <yutengwu@yahoo.com>
2023-07-09 13:40:11 -0700

    adjust home page

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,10 +1,12 @@
 import './index.scss'
 import Loader from 'react-loaders'
+import Brand from '../Home/Brand'
 
 const About = () => {
     return(
         <>
             <Loader type="pacman" />
+            <Brand />
             <div className='About'>
 
                 <h1>hi</h1>
