a3ff308e372da980c89ddae5b2f0985c27ff0d0c
Yuteng Terrence Wu <yutengwu@yahoo.com>
2023-07-09 16:03:49 -0700

    add content for about and contact page

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,18 +1,18 @@
 import './index.scss'
 import Loader from 'react-loaders'
 import Brand from '../Home/Brand'
-import America from '../../assets/images/america.jpg'
+
+
 const About = () => {
     return(
         <>
             <Loader type="pacman" />
-            <Brand />
+            <Brand />    
             <div className='about'>
                 <a href='https://www.youtube.com/watch?v=v_ru-UPLNN4' target="_blank" rel="noreferrer">
-                    <img className='about-america'src={America} alt="video"></img>
+                    <div className='about-america'></div>
                 </a>
             </div>
-           
         </>
         
        
